579c6f087988e1a26a21e048f89b1fc31bc16964
Gaurav Hegde <gaurav.hegde2021@vitstudent.ac.in>
2022-03-01 09:49:29 +0530

    PWA and New API

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // api key: c49b42a979b94006b59fcd057f708c67
+// new api: pub_5043062fe5333a7247cf52a648c5bf4c3e68
+//new api: ga70fV3mi6BtUKinu6VCLnTSkayq3TIxa_YoYJRLV1E
 
 let news_card = document.getElementById('news_card');
 
@@ -18,27 +20,63 @@ const registerSW = async () => {
 	}
 };
 
-fetch('https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=c49b42a979b94006b59fcd057f708c67')
-  .then(response => response.json())
-  .then(data => {
-    let articles = data.articles;
+var currentPage = 1 
+
+    fetch("https://api.newscatcherapi.com/v2/latest_headlines?&lang=en&topic=science&page_size=5&page=" +
+			currentPage,
+		{
+			method: "GET",
+			headers: {
+				"x-api-key": "ga70fV3mi6BtUKinu6VCLnTSkayq3TIxa_YoYJRLV1E",
+			},
+		}
+	)
+    .then(response => {console.log(response); return response.json()})
+    .catch(error => console.log(error))
+    .then(data => {
+        let articles = data.articles;
         console.log(articles);
         let newsHtml = "";
-        articles.forEach(function(element) {
+        data.articles.forEach((element) => {
             let news = `
                 <div class="card mx-auto my-5 text-white bg-dark mb-3 text-center" id="news" style="width: 20rem;">
-                    <img src="${element["urlToImage"]}" class="card-img-top img-fluid" alt="image">
-                    <div class="card-body">
-                        <h5 class="card-title"> ${element["title"]} </h5>
-                        <p class="card-text">${element["description"]}</p>
-                        <a href="${element["url"]}" class="btn btn-warning">Go somewhere</a>
-                    </div>
+                <img src="${element["media"]}" class="card-img-top img-fluid" alt="image">
+                <div class="card-body">
+                    <h5 class="card-title"> ${element["title"]} </h5>
+                    <p class="card-text">${element["excerpt"]}</p>
+                    <a href="${element["clean_url"]}" class="btn btn-warning">Go somewhere</a>
+                </div>
                 </div>
                 `;
             newsHtml += news;
-        });
-        news_card.innerHTML = newsHtml;
-  }).catch(error => console.log(error));
+    })
+            news_card.innerHTML = newsHtml;
+});
+        
+
+
+// fetch('https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=c49b42a979b94006b59fcd057f708c67')
+//   .then(response => {console.log(response); return response.json()})
+//   .catch(error => console.log(error))
+//   .then(data => {
+//     let articles = data.articles;
+//         console.log(articles);
+//         let newsHtml = "";
+//         articles.forEach(function(element) {
+//             let news = `
+//                 <div class="card mx-auto my-5 text-white bg-dark mb-3 text-center" id="news" style="width: 20rem;">
+//                     <img src="${element["urlToImage"]}" class="card-img-top img-fluid" alt="image">
+//                     <div class="card-body">
+//                         <h5 class="card-title"> ${element["title"]} </h5>
+//                         <p class="card-text">${element["description"]}</p>
+//                         <a href="${element["url"]}" class="btn btn-warning">Go somewhere</a>
+//                     </div>
+//                 </div>
+//                 `;
+//             newsHtml += news;
+//         });
+//         news_card.innerHTML = newsHtml;
+//   });
 
 
 
